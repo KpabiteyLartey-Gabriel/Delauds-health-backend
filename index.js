7b6945b47b01fe805077e53b0d9284e2e197a5db
klargdev <klargdev@gmail.com>
2025-07-24 19:07:20 +0000

    Refactor CORS options in index.js to allow dynamic origin handling and update start script in package.json

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const cors = require('cors');
 
 const app = express();
 
+const allowedOrigins = ['https://delauds-health.vercel.app'];
 const corsOptions = {
-  origin: 'https://delauds-health.vercel.app',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 };
 app.use(cors(corsOptions));
